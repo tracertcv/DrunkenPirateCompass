a56a5fa95fe7db832e4dc5129ff3aff0c6367954
Owen <owenledvina@gmail.com>
2015-07-15 03:06:25 -0400

    Added compass drawing.  Drawing angle calculation needs to be fixed as well as at least one major lifecycle bug for onResume.

--- app/src/main/java/com/ballmerpeakindustries/tracer/drunkenpiratecompass/MainActivity.java
+++ app/src/main/java/com/ballmerpeakindustries/tracer/drunkenpiratecompass/MainActivity.java
@@ -11,6 +11,8 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import java.net.URLEncoder;
@@ -19,130 +21,126 @@ import java.util.ArrayList;
 
 public class MainActivity extends ActionBarActivity implements LocationListener, SensorEventListener, AsyncPlaceResponse {
 
+    //Update distance
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
+    //Update time
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1;
+    //Location manager
+    protected LocationManager locationManager;
     //Status flag
     boolean isGPSEnabled = false;
     boolean isNetworkEnabled = false;
     boolean canGetLocation = false;
-
     Location location;
     double bearing;
     double latitude;
     double longitude;
     ArrayList<Place> placeList = new ArrayList<Place>();
-
-    String locationString="0,0";
-    String radiusString="1000";
-    String typeString="bar|liquor_store";
+    String locationString = "0,0";
+    String radiusString = "1000";
+    String typeString = "bar|liquor_store";
     String apiKey = "AIzaSyBQrk0CbUkTETN4KPzxezRfOjOwoGWQW28";
     String lastJSONResponse = "";
-
     SensorManager mSensorManager;
     Sensor accel;
     Sensor magnet;
     float mGravity[];
     float mGeomag[];
+    DrawContentSurfaceView drawView;
 
-    //Update distance
-    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
-    //Update time
-    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1;
-
-    //Location manager
-    protected LocationManager locationManager;
-
-    private Location updateLocation(){
+    private Location updateLocation() {
 
         Location mLoc;
-        if(this.canGetLocation){
-           mLoc = this.getLocation();
-        }
-        else mLoc = null;
+        if (this.canGetLocation) {
+            mLoc = this.getLocation();
+        } else mLoc = null;
         this.stopUsingGPS();
         return mLoc;
     }
 
-    public Location getLocation(){
-        try{
+    public Location getLocation() {
+        try {
             locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
 
             isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
 
             isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 
-            if(!isGPSEnabled && !isNetworkEnabled){
+            if (!isGPSEnabled && !isNetworkEnabled) {
                 System.out.println("No trackers enabled.");
-            }
-            else{
+            } else {
                 this.canGetLocation = true;
-                if(isNetworkEnabled){
+                if (isNetworkEnabled) {
                     locationManager.requestLocationUpdates(
                             LocationManager.NETWORK_PROVIDER,
                             MIN_TIME_BW_UPDATES,
                             MIN_DISTANCE_CHANGE_FOR_UPDATES, this
                     );
-                    if(locationManager!=null){
+                    if (locationManager != null) {
                         location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-                        if(location != null){
+                        if (location != null) {
                             latitude = location.getLatitude();
                             longitude = location.getLongitude();
                         }
                     }
                 }
-                if(isGPSEnabled){
+                if (isGPSEnabled) {
                     locationManager.requestLocationUpdates(
                             LocationManager.GPS_PROVIDER,
                             MIN_TIME_BW_UPDATES,
                             MIN_DISTANCE_CHANGE_FOR_UPDATES, this
                     );
-                    if(locationManager!=null){
+                    if (locationManager != null) {
                         location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                        if(location != null){
+                        if (location != null) {
                             latitude = location.getLatitude();
                             longitude = location.getLongitude();
                         }
                     }
                 }
             }
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return location;
     }
 
-    public void stopUsingGPS(){
-        if(locationManager != null){
+    public void stopUsingGPS() {
+        if (locationManager != null) {
             locationManager.removeUpdates(this);
         }
     }
 
-    private void UpdatePlaces(){
-        try{
+    private void UpdatePlaces() {
+        try {
             String placeUrlString = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" +
-                    "location="+locationString+
-                    "&radius="+radiusString+
+                    "location=" + locationString +
+                    "&radius=" + radiusString +
                     "&sensor=true" +
-                    "&types="+URLEncoder.encode(typeString,"UTF-8")+
-                    "&key="+apiKey;
+                    "&types=" + URLEncoder.encode(typeString, "UTF-8") +
+                    "&key=" + apiKey;
 
             AsyncPlaceGetter mAsyncPlaceGetter = new AsyncPlaceGetter(placeList, location, lastJSONResponse);
             mAsyncPlaceGetter.delegate = this;
             mAsyncPlaceGetter.execute(placeUrlString);
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     @Override
-    public void onProcessFinish(ArrayList<Place> p, String lastJSON){
+    public void onProcessFinish(ArrayList<Place> p, String lastJSON) {
         placeList = p;
         lastJSONResponse = lastJSON;
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         accel = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         magnet = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
@@ -154,9 +152,10 @@ public class MainActivity extends ActionBarActivity implements LocationListener,
         Location myLoc = this.getLocation();
 
 
-
-        lat.setText(Double.toString(myLoc.getLatitude()));
-        lon.setText(Double.toString(myLoc.getLongitude()));
+        //lat.setText(Double.toString(myLoc.getLatitude()));
+        //lon.setText(Double.toString(myLoc.getLongitude()));
+        drawView = new DrawContentSurfaceView(this);
+        setContentView(drawView);
 
     }
 
@@ -170,7 +169,7 @@ public class MainActivity extends ActionBarActivity implements LocationListener,
 
     @Override
 
-    protected void onDestroy(){
+    protected void onDestroy() {
 
     }
 
@@ -199,17 +198,18 @@ public class MainActivity extends ActionBarActivity implements LocationListener,
         TextView places = (TextView) findViewById(R.id.places);
         location = this.getLocation();
 
-        lat.setText(Double.toString(location.getLatitude()));
-        lon.setText(Double.toString(location.getLongitude()));
-        tbearing.setText(Double.toString(bearing));
-        locationString=Double.toString(location.getLatitude())+","+Double.toString(location.getLongitude());
+        //lat.setText(Double.toString(location.getLatitude()));
+        //lon.setText(Double.toString(location.getLongitude()));
+        //tbearing.setText(Double.toString(bearing));
+        locationString = Double.toString(location.getLatitude()) + "," + Double.toString(location.getLongitude());
         UpdatePlaces();
-        if(placeList.size()>0)places.setText(placeList.get(0).name);
+
+        //if(placeList.size()>0)places.setText(placeList.get(0).name);
         System.out.println(placeList.toString());
     }
 
     @Override
-    public void onSensorChanged(SensorEvent event){
+    public void onSensorChanged(SensorEvent event) {
         try {
             if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
                 mGravity = event.values;
@@ -223,30 +223,40 @@ public class MainActivity extends ActionBarActivity implements LocationListener,
                     float orientation[] = new float[3];
                     SensorManager.getOrientation(R, orientation);
                     bearing = Math.toDegrees(orientation[0]);
+                    System.out.println(bearing);
+                    updateCompass();
                 }
             }
 
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    private void updateCompass() {
+        Location target = placeList.get(0).loc;
+        double cBearing = bearing;
+        double difference = Math.abs(bearing - location.bearingTo(target)) % 360.0;
+
+        drawView.setRotation(difference);
+    }
+
     @Override
-    public void onResume(){
+    public void onResume() {
         super.onResume();
         mSensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_NORMAL);
         mSensorManager.registerListener(this, magnet, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
-    public void onPause(){
+    public void onPause() {
         super.onPause();
         stopUsingGPS();
         mSensorManager.unregisterListener(this);
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy){
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
 
     @Override
